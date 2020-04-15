14fb6031813f2316b10f23bbb070f3be2e804a0e
izzen <izzen@localhost.localdomain>
2020-04-15 20:05:32 +0700

    Page 3&4

---
+++ app/src/main/java/com/example/latihan2/Almost_there.kt
@@ -0,0 +1,12 @@
+package com.example.latihan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Almost_there : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_almost_there)
+    }
+}
---
+++ app/src/main/java/com/example/latihan2/verify_account.kt
@@ -0,0 +1,12 @@
+package com.example.latihan2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class verify_account : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify_account)
+    }
+}
