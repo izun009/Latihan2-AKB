5a67c4b566ee0935fac65a98a4093bf10575ffed
izzen <izzen@localhost.localdomain>
2020-04-15 16:51:42 +0700

    Page 1

---
+++ app/src/main/java/com/example/latihan2/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.latihan2
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
